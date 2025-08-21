0b976d2136b830445a8e5d109d8bdd746c4c9ff8
Summertree987 <windseeker966@outlook.com>
2025-08-21 20:49:05 +0800

    chore: 添加 .gitignore 并修改 test 文件

--- test.py
+++ test.py
@@ -4,18 +4,14 @@ from bs4 import BeautifulSoup
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 }
-url = "https://www.baidu.com"
+url = "https://bulletin.nuist.edu.cn/wjgg.htm"
 res = requests.get(url, headers=headers)
 res.encoding = 'utf-8'
 
-print(res.status_code)
-
 soup = BeautifulSoup(res.text, 'lxml')
 
-title = soup.title.string
-print(title)
-
 links = soup.find_all('a')
-for x in links:
-    l = x.get('href')
-    print(l)
+#for x in links:
+#   print(x.get_text())
+print(links)
+
