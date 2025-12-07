f18ad5b6446a36fcfe13d530d020344a87cfc7fb
Summertree987 <windseeker966@gmail.com>
2025-12-07 21:19:47 +0800

    12.7

--- C/test.cpp
+++ C/test.cpp
@@ -1,1 +1,15 @@
-﻿
+﻿struct Base1 final
+{
+};
+struct Derived1 : Base1
+{
+}; // 编译错误： Base1 为 final ，不允
+许被继承
+struct Base2
+{
+    virtual void f() final;
+};
+struct Derived2 : Base2
+{
+    void f(); // 编译错误： Base2::f 为 final ，不允许被覆盖
+};
